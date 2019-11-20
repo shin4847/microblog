aaaf5027ea3a2d520dc4f6032eac788ff218e112
EC2 Default User <ec2-user@ip-172-31-1-197.ec2.internal>
2019-11-20 15:25:49 +0000

    tenth commit

--- microblog/microblog/settings/heroku.py
+++ microblog/microblog/settings/heroku.py
@@ -4,12 +4,12 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['9dc001240c2442698429c68b17ed665e.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ['HTTP_X_FORWARDED_PHOTO', 'https']
